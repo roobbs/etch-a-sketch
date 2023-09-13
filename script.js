325ea0a89c78dfffb9415b6701c6edd7a08aa742
Carlos <carlossalmeroncabrera@gmail.com>
2023-09-12 21:50:36 -0600

    all functionalities working fine now

--- script.js
+++ script.js
@@ -1,57 +1,51 @@
-let defaultColor = "#277CA0";
-let defaultMode = "color";
-let defaultSize = 16; 
-//
-let currentColor = defaultColor;
-let currentMode = defaultMode;
-let currentSize = defaultSize;
-//
 let selectedColor = document.querySelector('.color');
 let colorBtn = document.querySelector('.colorBtn');
 let rainbowBtn = document.querySelector('.rainbowBtn');
 let eraserBtn = document.querySelector('.eraserBtn');
 let clearBtn = document.querySelector('.clearBtn')
+let sizeDiv = document.querySelector('.size');
+let sizeInput = document.querySelector('.sizeInput');
 let square = document.querySelector('.square');
 //
+let currentMode = "rainbow";
+//
+colorBtn.addEventListener("click", () => currentMode="color");
+rainbowBtn.addEventListener("click", () => currentMode="rainbow");
+eraserBtn.addEventListener("click", () => currentMode="eraser");
+clearBtn.addEventListener("click", () => {
+    createGridItems(sizeInput.value);
+    if (currentMode==="eraser") {
+        currentMode= "color"
+    }
+});
+//
 function createGridItems (size) {
+    sizeDiv.textContent = size+"x"+size;
+    square.innerHTML = ""; 
     square.style.cssText = "grid-template-columns: repeat("+size+", 1fr) ; grid-template-rows: ("+size+", 1fr);";
-    
     for (let i = 0; i< size*size ; i++) {
         let gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
         square.appendChild(gridItem);
+        gridItem.addEventListener("mouseenter", () => {
+            switch (currentMode) {
+                case "color":
+                    gridItem.style.cssText="background-color:"+selectedColor.value+"; border: 0px; border-radius: 0px";
+                    break;
+                case "rainbow":
+                    let r = Math.floor(Math.random() *256);
+                    let g = Math.floor(Math.random() *256);
+                    let b = Math.floor(Math.random() *256);
+                    gridItem.style.cssText="background-color:rgb("+r+","+g+","+b+"); border: 0px; border-radius: 0px";
+                    break;
+                case "eraser":
+                    gridItem.style.cssText= "";
+                    break;
+            }
+        });
     }
 }
 //
-function clearGrid () {
-    square.innerHTML = "";    
-}
+sizeInput.addEventListener('input', () => createGridItems(sizeInput.value));
 //
-function newCurrentColor (color) {
-    currentColor = color;
-}
-//
-function newCurrentMode (mode) {
-    switch (currentMode) {
-        case "color": 
-            //remover class de nombre que yo elija
-            break;
-        case "rainbow":
-            break;
-        case "eraser":
-            break;
-    } 
-    switch (mode) {
-        case "color": 
-            //agregar class del nombre que yo elija
-            break;
-        case "rainbow":
-            break;
-        case "eraser":
-            break;
-    }
-    currentMode = mode;
-}
-/*
-crear una funcion para dibujar los cuadros creados en square
-*/
+createGridItems(sizeInput.value);
